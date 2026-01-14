77a00db8e7f20be7a0ea6ec03b6d096886da995d
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-01-14 11:15:04 -0800

    using username instead

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ function Layout({ children }: { children: ReactNode }) {
       <footer className="bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 mt-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
           <p className="text-center text-sm text-gray-600 dark:text-gray-400">
-            Senior Intensive Project by Joshua Kakinuki - Demonstrating CS Data Structures in Automotive ECUs
+            Senior Intensive Project by Joshkaki00 - Demonstrating CS Data Structures in Automotive ECUs
           </p>
         </div>
       </footer>
